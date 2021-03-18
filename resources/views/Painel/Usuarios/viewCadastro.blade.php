1017141fc9ad4505f5e1d0ae066d300a11f52894
Geovane <geovanerrof@gmail.com>
2021-03-17 23:05:44 -0300

    StudioMensagensV1 comit 3

--- resources/views/Painel/Usuarios/viewCadastro.blade.php
+++ resources/views/Painel/Usuarios/viewCadastro.blade.php
@@ -1,54 +1,109 @@
 @extends ('Painel.Layout.index')
 
 
-@section('title', 'Cadastro Funcionário')
+@section('title', 'Cadastro Cliente')
 
 @section('content')
 
-<body class="hold-transition register-page">
-<div class="register-box">
-  <div class="register-logo">
-    <a><b> Cadastro </b> Funcionário</a>
+<br>
+<br>
+
+<div class="box box-info">
+  <div class="container">
+    
+    
+            <div class="box-header with-border">
+              <h3 class="text-center"> <a><b> Cadastro </b> Funcionário</a> </h3>
+            </div>
+
+            
+            <div class="box-body">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-address-card-o"></i></span>
+                      <input type="text" class="form-control" placeholder="Nome">         
+                    </div><br>
+                    
+                    
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-address-card"></i></span>
+                      <div><input type="text" class="form-control" placeholder="CPF"> </div>
+                    </div><br>
+
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                      <div><input type="text" class="form-control" placeholder="Senha"> </div>
+                    </div><br>
+
+                    
+                  
+
+                    
+                          <div class="row">
+                              <div class="col-xs-2">
+                              <i class="fa fa-phone"></i></span>
+                              <label for=""> Telefone:</label> <input type="text" class="form-control" placeholder="Rua:">
+                              </div>
+                            
+                              <div class="col-xs-2">
+                              <i class="fa fa-mobile"></i></span>
+                              <label for="">  Celular:</label> <input type="text" class="form-control" placeholder="Nº">
+                              </div>     
+                              
+                              <div class="col-xs-3">
+                              <i class="fa fa-calendar"></i>
+                              <label for=""> Data Nascimento:</label> <input type="text" class="form-control pull-right" id="datepicker">
+                              </div>
+
+                  
+                              <div class="col-xs-2">
+                                <i class="fa fa-female"></i>
+                                <i class="fa fa-male"></i>
+                                <label for=""> Gênero:</label>
+                                <select class="form-control select2" style="width: 100%;">
+                                      <option selected="selected">Nao Informar</option>
+                                      <option>Feminino</option>
+                                      <option>Masculino</option>
+                               
+                                    </select>                          
+                               </div>
+
+                               <div class="col-xs-2">
+                                <i class="fa fa-female"></i>
+                                <i class="fa fa-male"></i>
+                                <label for=""> Função:</label>
+                                <select class="form-control select2" style="width: 100%;">
+                                      <option selected="selected"> Administrador </option>
+                                      <option>Gerente</option>
+                                      <option>Colaborador</option>
+                               
+                                    </select>                          
+                               </div>
+
+
+                          </div>
+
+              
+
+                          
+                            <div class="box-footer with-footer">
+                              <h3 class="text-center">                               
+                              <button type="submit" class="btn btn-success">Cadastrar</button>
+                              <button type="submit" class="btn btn-danger">Cancelar</button>
+                            </h3>
+                            </div>       
+
+            
+            </div>
+            
+   
+      
+
+  
+  
   </div>
-
-  <div class="register-box-body">
-    <p class="login-box-msg"> Informe os dados:</p>
-
-    <form action="../../index.html" method="post">
-      <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Nome ...">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Usuário ...">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Senha ...">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Repita a senha">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-6">
-        <button type="submit" class="btn btn-info btn-block">Cadastrar</button>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-6">
-          <button type="submit" class="btn btn-danger btn-block">Cancelar</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-
-  </div>
-  <!-- /.form-box -->
 </div>
 
 
-</body>
 
 @endsection
 
--- resources/views/Painel/Usuarios/viewCliente.blade.php
+++ resources/views/Painel/Usuarios/viewCliente.blade.php
@@ -11,113 +11,113 @@
 <div class="box box-info">
   <div class="container">
     
+    
             <div class="box-header with-border">
               <h3 class="text-center"> <a><b> Cadastro </b> Cliente</a> </h3>
             </div>
 
+            
             <div class="box-body">
-                <div class="input-group">
-                <span class="input-group-addon"> <i class="fa fa-address-card-o"></i></span>
-                <input type="text" class="form-control" placeholder="Nome">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-address-card-o"></i></span>
+                      <input type="text" class="form-control" placeholder="Nome">         
+                    </div><br>
+                    
+                    
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-home"></i></span>
+                      <div><input type="text" class="form-control" placeholder="Endereço"> </div>
+                    </div><br>
+
+
+                    
+                        <div class="row">
+                          <div class="col-xs-8">
+                           <label for=""> Endereço:</label> <input type="text" class="form-control" placeholder="Rua:">
+                          </div>
+                        
+                          <div class="col-xs-2">
+                          <label for=""> Número:</label> <input type="text" class="form-control" placeholder="Nº">
+                          </div>
+
+                          <div class="col-xs-2">
+                          <label for=""> Bairro:</label>
+                          <select class="form-control select2" style="width: 100%;">
+                                <option selected="selected">Asa Branca</option>
+                                <option>Aparecida</option>
+                                <option>Buritis</option>
+                                <option>Bela Vista</option>
+                                <option>Centro</option>
+                                <option>Caimbé</option>
+                                <option>Caranã</option>
+                              </select>                          
+                          </div>
+                        </div> <br>
+
+                          <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-street-view"></i></span>
+                            <div><input type="text" class="form-control" placeholder="Referência"> </div>
+                          </div><br>
+
+                    
+                          <div class="row">
+                              <div class="col-xs-2">
+                              <i class="fa fa-phone"></i></span>
+                              <label for=""> Telefone:</label> <input type="text" class="form-control" placeholder="Rua:">
+                              </div>
+                            
+                              <div class="col-xs-2">
+                              <i class="fa fa-mobile"></i></span>
+                              <label for="">  Celular:</label> <input type="text" class="form-control" placeholder="Nº">
+                              </div>     
+                              
+                              <div class="col-xs-3">
+                              <i class="fa fa-calendar"></i>
+                              <label for=""> Data Nascimento:</label> <input type="text" class="form-control pull-right" id="datepicker">
+                              </div>
+
+                  
+                              <div class="col-xs-2">
+                                <i class="fa fa-female"></i>
+                                <i class="fa fa-male"></i>
+                                <label for=""> Gênero:</label>
+                                <select class="form-control select2" style="width: 100%;">
+                                      <option selected="selected">Nao Informar</option>
+                                      <option>Feminino</option>
+                                      <option>Masculino</option>
+                               
+                                    </select>                          
+                               </div>
+                          </div>
+
+                          <br>
+                          <div class="form-group">
+                             <i class="fa fa-file-text-o"></i>
+                             <label>Observação: </label>
+                             <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                          </div>
+
+
+                          
+                            <div class="box-footer with-footer">
+                              <h3 class="text-center">                               
+                              <button type="submit" class="btn btn-success">Cadastrar</button>
+                              <button type="submit" class="btn btn-danger">Cancelar</button>
+                            </h3>
+                            </div>       
+
+            
             </div>
-            <br>
-
-              
-               <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div>
-              <br>
-
-              <div class="input-group">
-                <input type="text" class="form-control">
-                <span class="input-group-addon"><i class="fa fa-check"></i></span>
-              </div>
-              <br>
-
-              <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-dollar"></i></span>
-                <input type="text" class="form-control">
-                <span class="input-group-addon"><i class="fa fa-ambulance"></i></span>
-              </div>
-
-              <h4>With checkbox and radio inputs</h4>
-
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="input-group">
-                        <span class="input-group-addon">
-                          <input type="checkbox">
-                        </span>
-                    <input type="text" class="form-control">
-                  </div>
-                  <!-- /input-group -->
-                </div>
-                <!-- /.col-lg-6 -->
-                <div class="col-lg-6">
-                  <div class="input-group">
-                        <span class="input-group-addon">
-                          <input type="radio">
-                        </span>
-                    <input type="text" class="form-control">
-                  </div>
-                  <!-- /input-group -->
-                </div>
-                <!-- /.col-lg-6 -->
-              </div>
-              <!-- /.row -->
-
-              <h4>With buttons</h4>
-
-              <p class="margin">Large: <code>.input-group.input-group-lg</code></p>
-
-              <div class="input-group input-group-lg">
-                <div class="input-group-btn">
-                  <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">Action
-                    <span class="fa fa-caret-down"></span></button>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                  </ul>
-                </div>
-                <!-- /btn-group -->
-                <input type="text" class="form-control">
-              </div>
-              <!-- /input-group -->
-              <p class="margin">Normal</p>
-
-              <div class="input-group">
-                <div class="input-group-btn">
-                  <button type="button" class="btn btn-danger">Action</button>
-                </div>
-                <!-- /btn-group -->
-                <input type="text" class="form-control">
-              </div>
-              <!-- /input-group -->
-              <p class="margin">Small <code>.input-group.input-group-sm</code></p>
-
-              <div class="input-group input-group-sm">
-                <input type="text" class="form-control">
-                    <span class="input-group-btn">
-                      <button type="button" class="btn btn-info btn-flat">Go!</button>
-                    </span>
-              </div>
-              <!-- /input-group -->
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-
+            
+   
       
 
+  
+  
   </div>
-
-
-
 </div>
+
+
 
 @endsection
 
