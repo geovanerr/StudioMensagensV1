79f9445a354dc650cf762835cc7b3f2b00b13e4e
SEFAZRR\geovane.gomes <geovane.gomes@sefaz.rr.gov.br>
2021-05-24 13:14:04 -0400

    StudioMensagensV1 V32a

--- resources/views/PDF/ordens.blade.php
+++ resources/views/PDF/ordens.blade.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-<table>
-<head>
- 
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <h2 style="text-align: center">Relatório - Ordens de Serviços</h2>
+
+<table border="1" cellpadding="0" cellspacing="0" style="width: 100%">
+
+    <thead>
+
     <title>Ordens Servicos</title>
+
     <tr>
         <th>ID</th>
         <th>Cliente</th>
         <th>Receptor</th>
         <th>Servicos</th>
-        <th>Status</th>    
+        <th>Status</th>
+        <th>Data OS</th>
     </tr>
-</head>
+</thead>
 <body>
     @foreach ($ordens as $ordem)
     <tr>
@@ -26,9 +32,11 @@
          <li> {{ $servico->servico }}</li>
      @endforeach
     </ul></td>
-    <td>{{$ordem->status}}</td> 
-    </tr>                                                              
-    
+    <td>{{$ordem->status}}</td>
+    <td>{{$ordem->dataos->format('d/m/Y')}}</td>
+
+    </tr>
+
     @endforeach
 </body>
 </table>
